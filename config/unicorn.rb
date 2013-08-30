2dfa035434218252ac83679367729c50c9298c34
himangshuj <himangshu@sokratik.com>
2013-08-30 07:13:34 +0530

    config changes to unicorn for aws

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,7 +1,7 @@
 worker_processes 4
 timeout 30
-APP_PATH = "~/sokratik-code/cs_comments_service/"
+APP_PATH = "/local/discussion_service"
 working_directory APP_PATH
 stderr_path "/var/log/discussion/unicorn.stderr.log"
-stdout_path "/var/log/discussion/unicorn.stderr.log"
+stdout_path "/var/log/discussion/unicorn.stdout.log"
 
