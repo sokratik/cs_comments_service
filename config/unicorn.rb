9b4e75f3b63056b4d893b21c17816fefc8de64a5
himangshuj <himangshu@sokratik.com>
2013-09-01 08:41:04 +0530

    fixed logging location

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,6 @@ worker_processes 4
 timeout 30
 APP_PATH = "/local/discussion_service"
 working_directory APP_PATH
-stderr_path "/var/log/discussion/unicorn.stderr.log"
-stdout_path "/var/log/discussion/unicorn.stdout.log"
+stderr_path "/work/logs/discussion/unicorn.stderr.log"
+stdout_path "/work/logs/discussion/unicorn.stdout.log"
 
